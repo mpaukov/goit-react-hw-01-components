6ce57214b893564a43d0e41a892c7071675a7a4a
Michael Paukov <michael.paukov@gmail.com>
2022-01-18 18:30:00 +0100

    start task1

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Profile from './components/Profile';
+
+export default function App() {
+  return (
+    <div>
+      <Profile
+        name={user.name}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+    </div>
+  );
+}
---
+++ src/components/Profile.js
@@ -0,0 +1,31 @@
+import data from './data/user.json';
+
+export default Profile(props) = (
+  <div class="profile">
+    <div class="description">
+      <img
+        src="https://www.flaticon.com/svg/static/icons/svg/3135/3135715.svg"
+        alt="Аватар пользователя"
+        class="avatar"
+      />
+      <p class="name">Petra Marica</p>
+      <p class="tag">@pmarica</p>
+      <p class="location">Salvador, Brasil</p>
+    </div>
+
+    <ul class="stats">
+      <li>
+        <span class="label">Followers</span>
+        <span class="quantity">1000</span>
+      </li>
+      <li>
+        <span class="label">Views</span>
+        <span class="quantity">2000</span>
+      </li>
+      <li>
+        <span class="label">Likes</span>
+        <span class="quantity">3000</span>
+      </li>
+    </ul>
+  </div>
+);
